327d09eca93c37a80a62a463068e5927aedb4cd8
Inozpavel <Inozpavel@mail.ru>
2021-02-22 14:19:03 +0300

    Added users database interface and user class

---
+++ Purchases/Data/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Purchases.Entities;
+
+namespace Purchases.Data
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> Users { get; }
+
+        User? FindUser(int id);
+
+        void AddUser(User user);
+    }
+}
---
+++ Purchases/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Purchases.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
