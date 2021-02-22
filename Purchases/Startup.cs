c740dc016cb909138bed9db78ccc200ddff5b28c
Inozpavel <Inozpavel@mail.ru>
2021-02-22 21:50:30 +0300

    Configured authentication scheme

--- Purchases/Startup.cs
+++ Purchases/Startup.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Purchases.Data;
 using Purchases.Services;
@@ -10,6 +15,10 @@ namespace Purchases
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -17,13 +26,29 @@ namespace Purchases
             services.AddSwaggerGen(options =>
             {
                 options.EnableAnnotations();
-
+                
                 options.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "PurchasesService",
                     Description = "Service for managing users and purchases",
                     Version = "v1"
                 });
+            });
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecretJWTKey"]))
+                };
             });
 
             services.AddSingleton<IUserRepository, MemoryUserRepository>();
@@ -37,14 +62,18 @@ namespace Purchases
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
                 options.RoutePrefix = "";
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "PurchasesService");
             });
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
