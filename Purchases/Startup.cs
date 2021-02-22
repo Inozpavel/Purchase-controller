c8018a71ce9f4a94181b98cbd455156f0184ea9b
Inozpavel <Inozpavel@mail.ru>
2021-02-22 19:59:58 +0300

    Installed and configured swagger packages

--- Purchases/Startup.cs
+++ Purchases/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Purchases.Data;
 using Purchases.Services;
 
@@ -12,6 +13,18 @@ namespace Purchases
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations();
+
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PurchasesService",
+                    Description = "Service for managing users and purchases",
+                    Version = "v1"
+                });
+            });
 
             services.AddSingleton<IUserRepository, MemoryUserRepository>();
             services.AddSingleton<IUserService, UserService>();
@@ -26,6 +39,12 @@ namespace Purchases
 
             app.UseRouting();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = "";
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "PurchasesService");
+            });
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
