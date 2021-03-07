82eff5042c7ca400c7106ecee9c19ac66bbfce84
Inozpavel <inozpavel@mail.ru>
2021-03-07 23:15:55 +0300

    Improved user profile

--- Purchases/Mapper/UserProfile.cs
+++ Purchases/Mapper/UserProfile.cs
@@ -9,11 +9,7 @@ namespace Purchases.Mapper
         public UserProfile()
         {
             CreateMap<RegisterRequest, User>()
-                .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dst => dst.Password, opt => opt.Ignore())
-                .ForMember(dst => dst.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(dst => dst.LastName, opt => opt.MapFrom(src => src.LastName))
-                .ForMember(dst => dst.Patronymic, opt => opt.MapFrom(src => src.Patronymic));
+                .ForMember(dst => dst.Password, opt => opt.Ignore());
         }
     }
 }
