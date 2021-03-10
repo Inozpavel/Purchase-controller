65a6baef27202c27f46e84c2a5187e627cf9c57f
Inozpavel <inozpavel@mail.ru>
2021-03-10 21:32:42 +0300

    Added migration for table Categories

---
+++ Stores/Migrations/20210307214727_AddedTableStoreCategories.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Stores.Migrations
+{
+    public partial class AddedTableStoreCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "Stores",
+                table => new
+                {
+                    StoreId = table.Column<int>("integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StoreName = table.Column<string>("text", nullable: false),
+                    Address = table.Column<string>("text", nullable: false),
+                    Phone = table.Column<string>("text", nullable: false)
+                },
+                constraints: table => { table.PrimaryKey("PK_Stores", x => x.StoreId); });
+
+            migrationBuilder.CreateTable(
+                "Products",
+                table => new
+                {
+                    ProductId = table.Column<int>("integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StoreId = table.Column<int>("integer", nullable: false),
+                    ProductName = table.Column<string>("text", nullable: false),
+                    Description = table.Column<string>("text", nullable: true),
+                    Price = table.Column<decimal>("numeric", nullable: false),
+                    CountInStock = table.Column<int>("integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        "FK_Products_Stores_StoreId",
+                        x => x.StoreId,
+                        "Stores",
+                        "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "StoreCategories",
+                table => new
+                {
+                    StoreCategoryId = table.Column<int>("integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StoreId = table.Column<int>("integer", nullable: false),
+                    StoreCategoryName = table.Column<string>("text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreCategories", x => x.StoreCategoryId);
+                    table.ForeignKey(
+                        "FK_StoreCategories_Stores_StoreId",
+                        x => x.StoreId,
+                        "Stores",
+                        "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "ProductStoreCategory",
+                table => new
+                {
+                    CategoriesStoreCategoryId = table.Column<int>("integer", nullable: false),
+                    ProductsProductId = table.Column<int>("integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductStoreCategory",
+                        x => new {x.CategoriesStoreCategoryId, x.ProductsProductId});
+                    table.ForeignKey(
+                        "FK_ProductStoreCategory_Products_ProductsProductId",
+                        x => x.ProductsProductId,
+                        "Products",
+                        "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_ProductStoreCategory_StoreCategories_CategoriesStoreCategor~",
+                        x => x.CategoriesStoreCategoryId,
+                        "StoreCategories",
+                        "StoreCategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                "IX_Products_StoreId",
+                "Products",
+                "StoreId");
+
+            migrationBuilder.CreateIndex(
+                "IX_ProductStoreCategory_ProductsProductId",
+                "ProductStoreCategory",
+                "ProductsProductId");
+
+            migrationBuilder.CreateIndex(
+                "IX_StoreCategories_StoreId",
+                "StoreCategories",
+                "StoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                "ProductStoreCategory");
+
+            migrationBuilder.DropTable(
+                "Products");
+
+            migrationBuilder.DropTable(
+                "StoreCategories");
+
+            migrationBuilder.DropTable(
+                "Stores");
+        }
+    }
+}
